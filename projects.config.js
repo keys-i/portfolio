23baa8ddd18f3ca4e19c812a5f7b7857911bf936
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-19 03:27:15 +0530

    updates to project

---
+++ projects.config.js
@@ -0,0 +1,315 @@
+export const project_list = [
+    // {
+    //     name: "",
+    //     date: "April 2023",
+    //     link: "",
+    //     description: [
+    //     ],
+    //     domains: [""]
+    // },
+    {
+        name: "CrowdFunding | Support Campaigns with Ethereum",
+        date: "April 2023",
+        link: "https://github.com/CERTIFIED2003/Crowdfunding-Blockchain",
+        description: [
+            "CrowdFunding allows users to create and fund projects using Ethereum, ensuring transparency and security in the crowdfunding process."
+        ],
+        domains: [
+            "solidity",
+            "tailwindcss",
+            "ethers.js"
+        ]
+    },
+    {
+        name: "Krypt | Blockchain Transaction",
+        date: "April 2023",
+        link: "https://github.com/CERTIFIED2003/Krypt-Blockchain-Transaction",
+        description: [
+            "Krypt is a personal project that documents my initial foray into the world of Web3 development. Transfer of Sepolia Ethereum and post a message with gif to be stored in Blockchain network."
+        ],
+        domains: [
+            "solidity",
+            "tailwindcss",
+            "ethers.js"
+        ]
+    },
+    {
+        name: "ToDo App",
+        date: "April 2023",
+        link: "https://github.com/CERTIFIED2003/ToDo-Internship",
+        description: [
+            "Full Stack Todo Application. Frontend made with Vite and MongoDB used as Database for Backend. Used Redux for CRUD applications"
+        ],
+        domains: [
+            "redux",
+            "mongodb",
+            "sass"
+        ]
+    },
+    {
+        name: "CertyStore | E-Commerce Site",
+        date: "March 2023",
+        link: "https://github.com/CERTIFIED2003/ECommerce-Site-v2",
+        description: [
+            "Full Stack E-Commerce Web App with fully functional Admin's Dashboard"
+        ],
+        domains: [
+            "mern",
+            "express",
+            "reactjs",
+            "node",
+            "redux",
+            "tailwindcss"
+        ]
+    },
+    {
+        name: "Login with IP System",
+        date: "March 2023",
+        link: "https://github.com/CERTIFIED2003/Login-with-IP-System",
+        description: [
+            "A simple authentication system. Login for Student, Teacher & Admin. For Student authentication, there is a predefined IP Address env variable assigned in the backend (NETWORK_IP)."
+        ],
+        domains: [
+            "mern",
+            "express",
+            "reactjs",
+            "node"
+        ]
+    },
+    {
+        name: "Twitter Clone",
+        date: "Feb 2023",
+        link: "https://github.com/CERTIFIED2003/Twitter-Clone",
+        description: [
+            "Awesome looking Twitter clone build using NextJS + TailwindCSS. Firebase v9 integrated for post creation and file storage. With the support of NewsAPI making this application super awesome.",
+        ],
+        domains: [
+            "nextjs",
+            "firebase-auth",
+            "firebase-database"
+        ]
+    },
+    {
+        name: "Reddit Clone",
+        date: "Feb 2023",
+        link: "https://github.com/CERTIFIED2003/Reddit-Clone",
+        description: [
+            "An awesome Reddit clone made with nextjs framework, used Chakra UI for CSS design & Firebase services for entire application's NoSQL.",
+        ],
+        domains: [
+            "nextjs",
+            "typescript",
+            "chakra-ui",
+            "firebase-auth",
+            "firebase-database",
+            "firebase-storage"
+        ]
+    },
+    {
+        name: "Questions Answering AI",
+        date: "Jan 2023",
+        link: "https://github.com/CERTIFIED2003/question-answering-ai",
+        description: [
+            "Powerful Query answering AI tool build with Vite, OPENAI & Tailwindcss.",
+        ],
+        domains: [
+            "vite",
+            "openai",
+            "tailwindcss",
+            "javascript"
+        ]
+    },
+    {
+        name: "Image Generator AI",
+        date: "Jan 2023",
+        link: "https://github.com/certified2003/image-generator-ai",
+        description: [
+            "Think of an awesome imagination and make it surreal with this awesome Image Generator AI",
+        ],
+        domains: [
+            "vite",
+            "tailwindcss",
+            "javascript"
+        ]
+    },
+    {
+        name: "UbuntuOS",
+        date: "Jan 2023",
+        link: "https://github.com/certified2003/ubuntuOS-playground",
+        description: [
+            "Website of theme Ubuntu 20.04, made using NEXT.js & Tailwind CSS",
+        ],
+        domains: [
+            "next.js",
+            "tailwindcss",
+            "javascript"
+        ]
+    },
+    {
+        name: "Clang Social v2",
+        noview: true,
+        date: "Dec 2022",
+        link: "https://clangsocial.netlify.app",
+        description: [
+            "A free-speech social media app where user can video chat, share their thoughts, images, follow and add friends. So what are you waiting for. Dive into this cool social media platform.",
+        ],
+        domains: [
+            "reactjs",
+            "mongodb",
+            "javascript"
+        ]
+    },
+    {
+        name: "Clang Social App",
+        noview: true,
+        date: "Jan 2023",
+        link: "https://play.google.com/store/apps/details?id=com.casuals4fun.clang_social",
+        description: [
+            "Clang Social mobile app to make life easier",
+        ],
+        domains: [
+            "java",
+            "android studio"
+        ]
+    },
+    {
+        name: "Clang Social v1",
+        noview: true,
+        date: "Nov 2022",
+        link: "https://clang-social.netlify.app",
+        description: [
+            "Powered by Sanity databse, this social media app can be used to share images to the community out there.",
+        ],
+        domains: [
+            "reactjs",
+            "sanity",
+            "javascript"
+        ]
+    },
+    {
+        name: "Keeper",
+        date: "Dec 2022",
+        link: "https://github.com/CERTIFIED2003/keeper_frontend",
+        description: [
+            "A very much Google Keep lookalike clone.",
+        ],
+        domains: [
+            "reactjs",
+            "javascript"
+        ]
+    },
+    {
+        name: "Infix Operations",
+        noview: true,
+        date: "Dec 2022",
+        link: "https://certified2003.github.io/infix_operation",
+        description: [
+            "An ultimate Infix Expressions problem solver. Over 10+ hours used to complete the project and bugs fix.",
+        ],
+        domains: [
+            "html",
+            "css",
+            "javascript"
+        ]
+    },
+    {
+        name: "HammerOBots",
+        noview: true,
+        date: "Jul 2022",
+        link: "https://play.google.com/store/apps/details?id=com.Casuals4Fun.HammerOBots",
+        description: [
+            "An epic battle between Mech robot and Evil Enemy Invasion on a island that has been abandoned and is deserted island long since.",
+        ],
+        domains: [
+            "unreal engine"
+        ]
+    },
+    {
+        name: "Face Recognition System + Attendance System",
+        date: "Aug 2022",
+        link: "https://github.com/CERTIFIED2003/FacialRecognitionSystem-withAttendance",
+        description: [
+            "This Project was made specifically for one of my Drone Projects to get Attendance of the acknowledged Personalities.",
+        ],
+        domains: [
+            "python"
+        ]
+    },
+    {
+        name: "Face Mask Detection System",
+        date: "Aug 2022",
+        link: "https://github.com/CERTIFIED2003/Face-Mask-Detection",
+        description: [
+            "This Project was made to distinguish the individuals whether they are wearing Mask or not. The model of this is trained by the images of the people wearing mask and those who are without mask.",
+        ],
+        domains: [
+            "python"
+        ]
+    },
+    {
+        name: "Items Detector AI",
+        date: "Aug 2022",
+        link: "https://github.com/CERTIFIED2003/Items-Detector-AI",
+        description: [
+            "This Project can help identify the objects throughout the live feed and can be explicitly used for the Security.",
+        ],
+        domains: [
+            "python"
+        ]
+    },
+    {
+        name: "J.A.R.V.I.S",
+        date: "Aug 2022",
+        link: "https://github.com/CERTIFIED2003/VoiceDetection-AI",
+        description: [
+            "An intelligent voice recognition software to make life easier.",
+        ],
+        domains: [
+            "python"
+        ]
+    },
+    {
+        name: "Space Shooter Game",
+        date: "Sep 2022",
+        link: "https://github.com/CERTIFIED2003/py-game",
+        description: [
+            "A casual space arcade game made with the help of Python's 'pygame' library.",
+        ],
+        domains: [
+            "pygame",
+            "python"
+        ]
+    },
+    {
+        name: "Tic-Tac-Toe Game",
+        date: "Oct 2022",
+        link: "https://github.com/CERTIFIED2003/TicTacToe_Apk-usingJAVA",
+        description: [
+            "A basic android tic-tac-toe game.",
+        ],
+        domains: [
+            "java",
+            "android studio"
+        ]
+    },
+];
+
+export const tag_colors = {
+    "javascript": "yellow-300",
+    "firebase": "red-600",
+    "firestore": "red-500",
+    "firebase auth": "red-400",
+    "chrome-extension": "yellow-400",
+    "flutter": "blue-400",
+    "dart": "blue-500",
+    "react-native": "purple-500",
+    "html5": "pink-600",
+    "sass": "pink-400",
+    "tensorflow": "yellow-600",
+    "django": "green-600",
+    "python": "green-200",
+    "codeforces-api": "gray-300",
+    "tailwindcss": "blue-300",
+    "next.js": "purple-600",
+    "mongodb": "green-600",
+}
