8b2ec6a8ed012df9e71dc5f06512ca5e708ca6a8
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-19 04:54:04 +0530

    edits

--- components/apps/radhesh.js
+++ components/apps/radhesh.js
@@ -133,6 +133,12 @@ function About() {
         </>
     )
 }
+function Portfolio() {
+    return (
+        <iframe src="https://shubhamlal.pages.dev" frameBorder="0" title="My Portfolio" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
 function Education() {
     return (
         <>
