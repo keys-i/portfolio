769792a9f0ad39ea81ddced9a6807ea5685f93e2
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-19 17:11:03 +0530

    fix link

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github1s.com/radhesh1/portfolio/blob/HEAD/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://github1s.com/radhesh1/radhesh1" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
         // This was amazing af!
         // Credi is the link to the original repo: https://github.com/conwnet/github1s
     )
