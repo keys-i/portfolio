ab85f610452196b87b7dcd55f653289fc52efe01
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-20 06:41:16 +0530

    edit

--- components/apps/gedit.js
+++ components/apps/gedit.js
@@ -44,6 +44,7 @@ export class Gedit extends Component {
 
         const serviceID = process.env.NEXT_PUBLIC_SERVICE_ID;
         const templateID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+        // template_xs6t4ka
         const templateParams = {
             'name': name,
             'subject': subject,
